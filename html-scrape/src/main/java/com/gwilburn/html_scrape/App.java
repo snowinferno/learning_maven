344fe7951ca676fc3f0d5213c79e040f934cb64a
Greg Wilburn <greg@gwilburn.com>
2014-05-13 23:39:12 -0700

    Extracting webRequest class to facilitate making a request over the web using apache http components. Also fixed typo in AppTest.java

--- html-scrape/src/main/java/com/gwilburn/html_scrape/App.java
+++ html-scrape/src/main/java/com/gwilburn/html_scrape/App.java
@@ -1,9 +1,6 @@
 package com.gwilburn.html_scrape;
 
 import org.apache.http.HttpEntity;
-import org.apache.http.client.fluent.Content;
-import org.apache.http.client.fluent.Request;
-import org.apache.http.client.fluent.Response;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.utils.URIBuilder;
@@ -29,29 +26,17 @@ public class App {
             String webPath = "/search";
             HashMap<String, String> params = new HashMap<>();
             params.put("q","lmgtfy");
-            makeHTTPRequest(protocol, webHost, webPath, params);
+
+            WebRequest request = new WebRequest();
+            request.setWebProtocol(WebRequest.protocol.HTTPS);
+            request.addParameter("q","lmgtfy");
+            request.makeFluentRequest(protocol,webHost,webPath,params);
+//            makeHTTPRequest(protocol, webHost, webPath, params);
             makeManualHTTPRequest(protocol, webHost, webPath, params);
         } catch (Exception e) {
             System.out.println(e.toString());
+            e.printStackTrace();
         }
-    }
-
-    public static void makeHTTPRequest(String protocol, String host, String path, HashMap<String,String> params) throws URISyntaxException, IOException {
-        URIBuilder builder = new URIBuilder();
-        builder.setScheme(protocol).setHost(host).setPath(path);
-        if( params != null ){
-            Set<String> keys = params.keySet();
-            for( String key : keys ){
-                builder.addParameter(key,params.get(key));
-            }
-        }
-
-        URI uri = builder.build();
-        Response resp = Request.Get(uri).execute();
-//        HttpResponse response = resp.returnResponse();
-//        System.out.println(response.getStatusLine().getStatusCode());
-        Content content = resp.returnContent();
-        System.out.println(content);
     }
 
     public static void makeManualHTTPRequest(String protocol, String host, String path, HashMap<String,String> params) throws URISyntaxException, IOException {
---
+++ html-scrape/src/main/java/com/gwilburn/html_scrape/WebRequest.java
@@ -0,0 +1,134 @@
+package com.gwilburn.html_scrape;
+
+import org.apache.http.client.fluent.Content;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.client.fluent.Response;
+import org.apache.http.client.utils.URIBuilder;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Set;
+
+/**
+ * Created by Greg on 5/13/14.
+ */
+public class WebRequest {
+    private HashMap<String,String> parameters;
+    private String webProtocol;
+    private String host;
+    private String path;
+    public WebRequest(String url) {
+        if( url.startsWith("http") ){
+            webProtocol = url.toLowerCase().split(":")[0];
+            url = url.split("://")[1];
+        }
+        String[] parts = url.split("/");
+        this.host = parts[0];
+        for( int i = 1; i < parts.length; i++ ){
+            this.path += parts[i];
+        }
+    }
+
+    public WebRequest(){
+
+    }
+
+    public String getWebProtocol() {
+        return webProtocol;
+    }
+
+    public void setWebProtocol(protocol webProtocol) {
+        this.webProtocol = webProtocol.name().toLowerCase();
+    }
+
+    public HashMap<String, String> getParameters() {
+        return parameters;
+    }
+
+    public void setParameters(HashMap<String, String> parameters) {
+        this.parameters = parameters;
+    }
+
+    public WebRequest addParameter(String key, String value){
+        if( this.parameters == null )
+            this.parameters = new HashMap<>();
+        this.parameters.put(key, value);
+        return this;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public void makeFluentRequest(String protocol, String host, String path, HashMap<String,String> params) throws URISyntaxException, IOException {
+        this.webProtocol = protocol;
+        this.host = host;
+        this.path = path;
+        this.parameters = params;
+        this.makeFluentRequest();
+    }
+
+    public void makeFluentRequest() throws URISyntaxException, IOException {
+        checkRequestPieces();
+        URIBuilder builder = new URIBuilder();
+        builder.setScheme(this.webProtocol).setHost(this.host).setPath(this.path);
+        if( this.parameters != null ){
+            Set<String> keys = this.parameters.keySet();
+            for( String key : keys ){
+                builder.addParameter(key,this.parameters.get(key));
+            }
+        }
+
+        URI uri = builder.build();
+        Response resp = Request.Get(uri).execute();
+        Content content = resp.returnContent();
+        System.out.println(content);
+    }
+
+    private void checkRequestPieces() throws URISyntaxException {
+        if( this.host == null || this.path == null || this.webProtocol == null ){
+            String providedURL = this.webProtocol + "://" + this.host + "/" + this.path;
+
+            if( this.parameters == null )
+                providedURL += "?" + this.parameters;
+            else{
+                providedURL += "?";
+                Set<String> keys = this.parameters.keySet();
+                for( String key : keys ){
+                    providedURL += key + "=" + this.parameters.get(key) + "&";
+                }
+                providedURL = providedURL.replaceAll("&$","");
+            }
+
+            String reason = "";
+            if( this.host == null )
+                reason += "Host is null. ";
+            if( this.path == null )
+                reason += "Path is null. ";
+            if( this.webProtocol == null )
+                reason += "Protocol is null. ";
+
+            throw new URISyntaxException(providedURL, reason);
+        }
+    }
+
+    public enum protocol {
+        HTTP,
+        HTTPS
+    }
+
+}
